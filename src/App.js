0db1259131071e276af83b1541ccc34a69430637
SRC0747 <RjSrc@0747>
2021-10-19 00:18:41 +0530

    Welcome to HelloWorld React Js Designing Page.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <header> Hello World </header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>HelloWorld...!!!!!!!</code>
         </p>
         <a
           className="App-link"
